8894ff8e51bd8fd25a4e963dde2647216bef06c0
Noam Ross (Travis-CI) <ross@ecohealthalliance.org>
2019-02-16 11:22:29 -0500

    working cron script

---
+++ README.md
@@ -0,0 +1,5 @@
+# cranstats
+
+An always-updated database of summary statistics of CRAN packages.
+
+`update.sh/` mirrors all CRAN contributed source packages from ftp://cran.r-project.org/pub/R/src/contrib/ into `tarballs/` and decompresses them into `packages/` 
--- update.sh
+++ update.sh
@@ -1,3 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-rsync -tlzv  cran.r-project.org::CRAN/src/contrib/*tar.gz tarballs | tee /dev/tty | grep gz | xargs -i tar xf tarballs/{} -C packages/
+# Stuff to make this work in cron
+PATH="/bin:/usr/bin:/usr/local/bin:$PATH"  #Provide the path
+cd "${0%/*}"       #Set working directory to location of this script
+
+set -e  # stop script if error
+
+# Sync all source packages from CRAN
+rsync -tzm -i --no-links --dirs --delete --include="*.tar.gz" --exclude="*" cran.r-project.org::CRAN/src/contrib/ tarballs > logs/rsync_latest.log
+cp logs/rsync_latest.log logs/rsync_`date "+%Y_%m_%d__%H_%M_%S"`.log
+
+# Unzip newly updated packages
+echo "New/updated packages"
+cat logs/rsync_latest.log | grep ">" | grep -o "\S*$" 
+cat logs/rsync_latest.log | grep ">" | grep -o "\S*$" | xargs -i tar xfm tarballs/{} -C packages/
+echo "Deleted packages"
+
